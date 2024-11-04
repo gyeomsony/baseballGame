35ecacf8bec9b40a6f4eb40fdcc26814384a35fc
gyeom <gyeomii95@naver.com>
2024-11-04 16:15:22 +0900

    [feat] answer print 구문 추가

--- baseballGame/main.swift
+++ baseballGame/main.swift
@@ -4,24 +4,33 @@
 //
 //  Created by 손겸 on 11/4/24.
 //
+/*
+ Lv.1
+ - 1에서 9까지의 서로 다른 임의의 수 3개를 정하고 맞추는 게임입니다
+ - 정답은 랜덤으로 만듭니다.(1에서 9까지의 서로 다른 임의의 수 3자리)
+ */
 
 import Foundation
 
-class baseballGame {
+class BaseballGame {
     func start() {
-        // 정답 생성 호출
         let answer = makeAnswer()
+        print("정답은 \(answer) 입니다.")
+    }
+    func makeAnswer() -> [Int] {
+        // 랜덤으로 뽑기위한 빈 배열
+        var numbers: [Int] = []
+        // while문으로 3개 뽑을 때 까지 참으로 돌리고 random 메서드 이용해서 추출
+        while numbers.count < 3 {
+            let number = Int.random(in: 1...9)
+            numbers.append(number)
+        }
+        return numbers
     }
 }
-// 정답 번호 3개 랜덤 생성
-func makeAnswer() -> [Int] {
-    var numbers:[Int] = []
-    while numbers.count < 3{
-        var number = Int.random(in: 1...9)
-    }
-    return numbers
-}
-
-let game = baseballGame()
+// 인스턴스 생성
+let game = BaseballGame()
 game.start()
 
+// 정답 만드는 기능을 따로 빼보기
+// 클래스별로 나눠보기 = 시작하기,기록보기,힌트보기,번호 랜덤생성하기
