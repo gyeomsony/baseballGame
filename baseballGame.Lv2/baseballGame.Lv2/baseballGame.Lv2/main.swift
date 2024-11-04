945f08d207e96440a1d95a55d102daccb80df6ee
gyeom <gyeomii95@naver.com>
2024-11-04 22:50:30 +0900

    [feat] 숫자 중복 제거

---
+++ baseballGame.Lv2/baseballGame.Lv2/baseballGame.Lv2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  baseballGame.Lv2
+//
+//  Created by 손겸 on 11/4/24.
+//
+
+
+
+import Foundation
+
--- baseballGame/main.swift
+++ baseballGame/main.swift
@@ -4,11 +4,11 @@
 //
 //  Created by 손겸 on 11/4/24.
 //
-/*
- Lv.1
+/* Lv.1
  - 1에서 9까지의 서로 다른 임의의 수 3개를 정하고 맞추는 게임입니다
  - 정답은 랜덤으로 만듭니다.(1에서 9까지의 서로 다른 임의의 수 3자리)
  */
+
 
 import Foundation
 
@@ -18,16 +18,18 @@ class BaseballGame {
         print("정답은 \(answer) 입니다.")
     }
     func makeAnswer() -> [Int] {
-        // 랜덤으로 뽑기위한 빈 배열
-        var numbers: [Int] = []
+        // 중복 제거를 위한 Set
+        var numbers = Set<Int>()
+        
         // while문으로 3개 뽑을 때 까지 참으로 돌리고 random 메서드 이용해서 추출
         while numbers.count < 3 {
             let number = Int.random(in: 1...9)
-            numbers.append(number)
+            numbers.insert(number)
         }
-        return numbers
+        return Array(numbers)
     }
 }
+
 // 인스턴스 생성
 let game = BaseballGame()
 game.start()
